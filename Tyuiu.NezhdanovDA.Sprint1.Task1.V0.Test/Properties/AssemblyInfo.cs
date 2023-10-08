4953a4e072894ce8e9f0c105340318e646012882
Tyuiu.NezhdanovDA@outlook.com <Tyuiu.NezhdanovDA@outlook.com>
2023-10-08 22:27:35 +0500

    Task1

---
+++ Tyuiu.NezhdanovDA.Sprint1.Task1.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NezhdanovDA.Sprint1.Task1.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NezhdanovDA.Sprint1.Task1.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("361e8ab5-3de5-4074-8218-73381c4cf0ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.NezhdanovDA.Sprint1.Task1.V0/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NezhdanovDA.Sprint1.Task1.V0.Lib;
+namespace Tyuiu.NezhdanovDA.Sprint1.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Cпринт #1 | Выполнил: Нежданов Д. А. | СМАРT6-23-1";
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Cпринт #1                                                                  *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                           *");
+            Console.WriteLine("* Задание #1                                                                 *");
+            Console.WriteLine("* Вариант #0                                                                *");
+            Console.WriteLine("* Выполнил: Нежданов Дмитрий Алексеевич | СМАРT6-23-1                        *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные ,   *");
+            Console.WriteLine("* вычисляет результат по формуле x / (y - 1) и печатает его на экране.       *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+           
+            double x, y;
+           
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+            
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+
+        }
+    }
+}
